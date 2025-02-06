212748c0ca009fbee6977f04244571f686a3ea7e
Denisse <yuleiditho@gmail.com>
2025-02-06 11:49:09 -0600

    feat: Add script with primitive data type variables

---
+++ 01- Intro JS/tipo-de-datos.js
@@ -0,0 +1,38 @@
+console.log("Hello World");
+
+// Datos primitivos 
+let age = 10; // Number
+let pi = 3.1416; // Number
+let veryBigNumber = 12345n; // BigInt
+let name = "Denisse"; // String
+let month = 'February'; // String
+let value = true; // Boolean
+let valuePassword = false; // Boolean
+let data = null; // Null
+let password; // Undefined
+let uniqueKey = Symbol("4564515_key");  // Symbol
+
+
+// Imprimir en consola el tipo de dato de cada variable
+console.log(typeof (age)); // number
+console.log(typeof (pi)); // number
+console.log(typeof (veryBigNumber)); // bigint
+console.log(typeof (name)); // string
+console.log(typeof (month)); // string
+console.log(typeof (value)); // boolean
+console.log(typeof (valuePassword)); // boolean
+console.log(typeof (data)); // object
+console.log(typeof (password)); // undefined
+console.log(typeof (uniqueKey)); // symbol
+
+
+// Imprimir en consola
+console.log(age); 
+console.log(pi);
+console.log(veryBigNumber);
+console.log(name);
+console.log(value);
+console.log(valuePassword);
+console.log(data);
+console.log(password);
+console.log(uniqueKey);
---
+++ README.md
@@ -0,0 +1,20 @@
+# Módulo 3: Javascript
+
+**JavaScript** es el lenguaje de programación que hace que las páginas web sean interactivas. Desde efectos visuales y animaciones hasta la gestión de datos y la interacción con el usuario, JavaScript es el motor que impulsa la web moderna.
+
+
+## Clase 01- Introducción a JavaScript
+**Variables**: Son contenedores que almacenan valores. Pueden ser declaradas
+con `let`, `const` o `var`.
+
+**Datos primitivos**:  Son los tipos de datos más simples y directos que existen en el lenguaje
+- Números: Representa números, tanto enteros como decimales.
+- Strings: Representa cadenas de texto.
+- Booleans:  Representa valores lógicos: `true` (verdadero) o `false` (falso). Se utilizan para tomar decisiones en el código.
+- Null: Es un valor asingado explícitamente a una variable para indicar que no tiene un valor.
+- Undefined: Representa una variables a la que no se le ha asignado un valor.
+- bigInt: Se utiliza para representa números enteros muy grandes
+- Symbol: Se utiliza para crear identificadores únicos.
+
+
+
